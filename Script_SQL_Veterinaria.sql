4c5c6fb69ff6d0afe7cf036fe631ac67c05a8e6b
alonsohuertas <151574466+alonsohuertas@users.noreply.github.com>
2024-04-07 11:39:56 -0600

    sql

---
+++ Script_SQL_Veterinaria.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE Veterinaria
+
+
+CREATE TABLE Cliente
+PK: ID_Cliente, Atributos: Nombre, Dirección, Teléfono
+
+
+CREATE TABLE Mascota
+PK: ID_Mascota, Atributos: Nombre, Especie, Raza, Fecha_Nacimiento, ID_Cliente (FK)
+
+
+CREATE TABLE Personal
+PK: ID_Personal, Atributos: Nombre, Posición, Especialidad
+
+
+CREATE TABLE Servicio
+PK: ID_Servicio, Atributos: Descripción, Costo
+
+
+CREATE TABLE Cita
+PK: ID_Cita, Atributos: Fecha, Hora, ID_Cliente (FK), ID_Mascota (FK), ID_Personal (FK)
+
+
+CREATE TABLE Proveedor
+PK: ID_Proveedor, Atributos: Nombre, Dirección, Teléfono
+
+
+CREATE TABLE Producto
+PK: ID_Producto, Atributos: Nombre, Descripción, Costo, ID_Proveedor (FK)
