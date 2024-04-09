318ba09af116415cc59cfba15a9661400e460647
alonsohuertas <151574466+alonsohuertas@users.noreply.github.com>
2024-04-08 22:48:15 -0600

    Update Script_SQL_Veterinaria.sql

--- Script_SQL_Veterinaria.sql
+++ Script_SQL_Veterinaria.sql
@@ -1,28 +1,71 @@
 CREATE DATABASE Veterinaria
 
 
-CREATE TABLE Cliente
-PK: ID_Cliente, Atributos: Nombre, Dirección, Teléfono
+CREATE TABLE Cliente (
+Cedula VARCHAR(25),
+Nombre VARCHAR(50) NOT NULL,
+Apellido VARCHAR(50) NOT NULL,
+Direccion VARCHAR(100),
+Telefono VARCHAR(20),
+CONSTRAINT PK_Cliente PRIMARY KEY (Cedula)
+);
 
 
-CREATE TABLE Mascota
-PK: ID_Mascota, Atributos: Nombre, Especie, Raza, Fecha_Nacimiento, ID_Cliente (FK)
+
+CREATE TABLE Mascota (
+ID_Mascota INT,
+Nombre VARCHAR(50) NOT NULL,
+Especie VARCHAR(50),
+Raza VARCHAR(50),
+Genero CHAR(1),
+Fecha_Nacimiento DATE,
+Cedula_Cliente varchar(25),
+CONSTRAINT PK_Mascota PRIMARY KEY (ID_Mascota),
+CONSTRAINT FK_Mascota_Cliente FOREIGN KEY (Cedula_Cliente) REFERENCES Cliente (Cedula),
+CONSTRAINT CHK_Genero CHECK (Genero IN ('M', 'F'))
+);
 
 
-CREATE TABLE Personal
-PK: ID_Personal, Atributos: Nombre, Posición, Especialidad
+CREATE TABLE Empleado (
+Cedula VARCHAR(25),
+Nombre VARCHAR(50) NOT NULL,
+Apellido VARCHAR(50) NOT NULL,
+Posicion VARCHAR(50),
+CONSTRAINT PK_Empleado PRIMARY KEY (Cedula)
+);
 
 
-CREATE TABLE Servicio
-PK: ID_Servicio, Atributos: Descripción, Costo
+CREATE TABLE Servicio (
+ID_Servicio INT,
+Descripcion VARCHAR(100),
+Costo DECIMAL(10, 2),
+CONSTRAINT PK_Servicio PRIMARY KEY (ID_Servicio)
+);
 
 
-CREATE TABLE Cita
-PK: ID_Cita, Atributos: Fecha, Hora, ID_Cliente (FK), ID_Mascota (FK), ID_Personal (FK)
+CREATE TABLE Cita (
+ID_Cita INT,
+Fecha DATE,
+Hora TIME,
+Cedula_Cliente VARCHAR(25),
+ID_Mascota INT,
+Cedula_Empleado VARCHAR(25),
+CONSTRAINT PK_Cita PRIMARY KEY (ID_Cita),
+CONSTRAINT FK_Cita_Cliente FOREIGN KEY (Cedula_Cliente) REFERENCES Cliente (Cedula),
+CONSTRAINT FK_Cita_Mascota FOREIGN KEY (ID_Mascota) REFERENCES Mascota (ID_Mascota),
+CONSTRAINT FK_Cita_Empleado FOREIGN KEY (Cedula_Empleado) REFERENCES Empleado (Cedula)
+);
 
 
-CREATE TABLE Proveedor
-PK: ID_Proveedor, Atributos: Nombre, Dirección, Teléfono
+
+CREATE TABLE Proveedor (
+ID_Proveedor VARCHAR(25),
+Nombre VARCHAR(50) NOT NULL,
+Direccion VARCHAR(100),
+Telefono VARCHAR(20),
+CONSTRAINT PK_Proveedor PRIMARY KEY (ID_Proveedor)
+);
+
 
 
 CREATE TABLE Producto
